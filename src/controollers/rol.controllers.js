86f7440f19a5362b58f7ba5e28016ba5192f7924
EdvinCanastuj <ecanastujv@miumg.edu.gt>
2023-11-20 00:09:33 -0600

    apis para rol coompletos

--- src/controollers/rol.controllers.js
+++ src/controollers/rol.controllers.js
@@ -10,11 +10,53 @@ try {
 }
 };
 // metodo para insertar
+const addRol = async (req, res) => {
+try {
+    const {  tipo_rol } = req.body;
+    if ( !tipo_rol) {
+    res.status(400).json({ message: "Bad Request. Please fill all fields." });
+    return;
+    }
+    const rol = { tipo_rol };
+    const connection = await getConnection();
+    const result = await connection.query("INSERT INTO rol SET ?", rol);
+    res.json({ message: "Rol Added" });
+} catch (error) {
+    res.status(500).send(error.message);
+}
+};
+//metodo para eliminar
+const deleteRol = async (req, res) => {
+try {
+    const { id } = req.params;
+    const connection = await getConnection();
+    const result = await connection.query("DELETE FROM rol WHERE id_rol = ?;", [id]);
+    res.json(result);
+} catch (error) {
+    res.status(500).send(error.message);
+}
+};
 
 // metodo para actualizar
-
-// metodo para eliminar
+const updateRol = async (req, res) => {
+    try {
+    const { id_rol } = req.params;
+    const { tipo_rol } = req.body;
+    // if (!id_rol || !tipo_rol ) {
+    // return res.status(400).json({ message: "Bad Request. Please fill all fields." });
+    // }
+    const rol = { tipo_rol };
+    const connection = await getConnection();
+    const result = await connection.query("UPDATE rol SET ? WHERE id_rol = ?;", [rol, id_rol]);
+    res.json("updated successfully");
+} catch (error) {
+    res.status(500).send(error.message);
+}
+};
 
 export const methods = {
-    getRol
+    getRol,
+    addRol,
+    deleteRol,
+    updateRol
 }
--- src/controollers/usuario.controllers.js
+++ src/controollers/usuario.controllers.js
@@ -2,19 +2,50 @@ import { getConnection } from "./../db/database";
 const getUsuario = async (req, res) => {
     try {
         const connection = await getConnection();
-        const result = await connection.query("SELECT * FROM usuario;");
+        const result = await connection.query("SELECT u.nombre, u.apellido, r.tipo_rol FROM usuario u INNER JOIN rol r ON r.id_rol = u.tipo_rol ;");
         res.json(result);
     } catch (error) {
         res.status(500).send(error.message);
     }
     };
     // metodo para insertar
+    const addUsuario = async (req, res) => {
+        try {
+        const {
+        nombre,
+        apellido,
+        correo,
+        cel,
+        password,
+        tipo_rol
+        } = req.body;
+        const connection = await getConnection();
+          // Calculamos el valor total en la consulta SQL
+        const sql = "INSERT INTO usuario (nombre, apellido, correo, cel, password, tipo_rol) VALUES (?, ?, ?, ?, ?, ?);";
+        const result = await connection.query(sql, [
+            nombre,
+            apellido,
+            correo,
+            cel,
+            password,
+            tipo_rol
+        ]);
+        res.json({ message: "Usuario Added" });
+        } catch (error) {
+        console.error("Error al insertar usuario:", error);
+        res.status(500).json({ error: "Error interno del servidor" });
+        }
+    };
+    // metodo para actualizar
     
+    // metodo para eliminar
+
     // metodo para actualizar
     
     // metodo para eliminar
     
     export const methods = {
-        getUsuario
+        getUsuario,
+        addUsuario
     }
     
