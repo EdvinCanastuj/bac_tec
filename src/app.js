524cb3d2c4fae292693b9d81ec46b543f710ac9a
Edvin <ecanastujv@miumg.edu.gt>
2024-01-30 20:59:10 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import rolRouters from './routes/rol.routers.js';
 import usuarioRouters from './routes/usuario.routers.js';
-import estudianteRouters from './routes/estudiante.routers.js';
+
 const app = express();
 const cors = require('cors');
 
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use("/estudiante", estudianteRouters);
+
 app.use("/rol", rolRouters);
 app.use("/usuario", usuarioRouters);
 export default app;
